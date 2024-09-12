f6b99c80a52ce7397783d140d3edbaae8c71924b
Aureo Beck <aureosouza0801@gmail.com>
2024-09-12 16:54:07 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const [text, onChangeText] = React.useState('');
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            padding: 10,
+          }}>
+          <Text style={styles.sectionTitle}>
+            {'TextInput Copy/Paste Bug Demo'}
+          </Text>
+          <Text style={styles.sectionDescription}>
+            {
+              'Try copying and pasting a text bigger then 100 length and notice the red color style is not applied. Removing one character and adding back manually shows the red color.'
+            }
+          </Text>
+          <TextInput
+            onChangeText={onChangeText}
+            value={text}
+            style={[styles.input, text.length >= 100 && {color: 'red'}]}
+            maxLength={100}
+            multiline
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 200,
+    marginVertical: 12,
+    borderWidth: 1,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+});
+
+export default App;
